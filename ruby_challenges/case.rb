8d923a41a1669919f85ae06bad377390e1276fb5
thisismynightmare <ericabross@gmail.com>
2019-10-14 16:21:07 -0500

    modules 1-11

---
+++ ruby_challenges/case.rb
@@ -0,0 +1,15 @@
+weather_today = 'sunny'
+
+case weather_today
+when 'sunny'
+	puts "Shorts and sunglasses are needed today."
+when 'rainy'
+	puts "Grab a raincoat and umbrella."
+when 'foggy'
+	puts "You'll need a hat for all this dreariness."
+when 'cloudy'
+	puts "No sunglassess needed today!"
+else 
+	puts "I am not equipped to give you that kind of advice."
+end
+		
---
+++ ruby_challenges/first_method.rb
@@ -0,0 +1,6 @@
+def my_first_method(num1, num2)
+	result = num1 + num2
+	puts result
+end
+
+my_first_method(5, 10)
